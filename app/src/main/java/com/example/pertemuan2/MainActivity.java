d290b15f71d6af03610e0eb213acf41b1772b806
valentcap <valent.christian@ti.ukdw.ac.id>
2019-08-26 20:15:12 +0700

    commit pertama coba

---
+++ app/src/main/java/com/example/pertemuan2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pertemuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.edittextpicker.aliazaz.EditTextPicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditTextPicker ETP;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
